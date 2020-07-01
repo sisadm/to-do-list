8f91876d5486210b687533221041b3a08ba23c40
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2020-07-01 21:33:47 +0100

    event listener to btn-s.
    add/delete btn from list. Moving up and down.

---
+++ script.js
@@ -0,0 +1,76 @@
+const showList = document.getElementById('show');
+const listDiv = document.querySelector('.list');
+const listUl = document.querySelector('ul');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const listItem = document.getElementsByTagName('li');
+
+
+
+
+// hide & show list 
+showList.addEventListener('click', () => {
+    if(listDiv.style.display == "none") {
+        showList.textContent = "Hide list";
+        listDiv.style.display = "block";
+    } else {
+        listDiv.style.display = "none";
+        showList.textContent = "Show list";
+    }
+})
+
+// add item
+addItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.createElement('li');
+    if (addItemInput.value != "") {
+        li.innerHTML = `  <span class="liDiv notchecked">${addItemInput.value}</span> 
+        <button class="delete">Delete</button> 
+        <button class="up">&and;</button>
+        <button class="down">&or;</button>`;
+        ul.appendChild(li);
+        addItemInput.value = '';
+    } else {
+        alert("Please fill the input if you want to add something to list!");
+    };
+})
+
+
+
+// up & down & delete buttons
+listUl.addEventListener('click', (event) => {
+    if(event.target.tagName == 'BUTTON') {  // target the buttons 
+        if(event.target.className == 'delete') {  // target the button with delete class
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+        if(event.target.className == 'up') {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            let prev = li.previousElementSibling;
+            if(prev) {
+                ul.insertBefore(li, prev);
+            }
+        }
+        if(event.target.className == 'down') {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            let next = li.nextElementSibling;
+            if(next) {
+                ul.insertBefore(next, li);
+            }
+        }
+    }
+    if(event.target.tagName == 'SPAN') {
+        if(event.target.classList.contains('checked')) {
+            event.target.classList.replace('checked', 'notchecked');
+        } 
+        else {
+            event.target.classList.replace('notchecked', 'checked');
+        } 
+    }
+})
+
+
+
